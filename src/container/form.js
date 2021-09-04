4c54653821026403c9069b57ba0a4c56d09abaa4
suraj480 <surajmaurya991@gmail.com>
2021-09-04 19:48:19 +0530

    first commit

---
+++ src/container/form.js
@@ -0,0 +1,75 @@
+import React from 'react'
+import { Form, Input, Button, Checkbox } from 'antd';
+import '../App.css'
+import 'antd/dist/antd.css';
+function form(){
+    return(
+        <div className="form">
+<Form
+      name="basic"
+      labelCol={{
+        span: 8,
+      }}
+      wrapperCol={{
+        span: 16,
+      }}
+      initialValues={{
+        remember: true,
+      }}  
+      autoComplete="off"
+    >
+      <Form.Item
+        label="Username"
+        name="username"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your username!',
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        label="Password"
+        name="password"
+        rules={[
+          {
+
+            required: true,
+            message: 'Please input your password!',
+          },
+        ]}
+      >
+        <Input.Password />
+      </Form.Item>
+
+      <Form.Item
+        name="remember"
+        valuePropName="checked"
+        wrapperCol={{
+          offset: 8,
+          span: 16,
+        }}
+      >
+        <Checkbox>Remember me</Checkbox>
+      </Form.Item>
+
+      <Form.Item
+        wrapperCol={{
+          offset: 8,
+          span: 16,
+        }}
+      >
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form.Item>
+     
+    </Form>
+
+        </div>
+    )
+}
+export default form;
